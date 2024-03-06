e6c31d3c7d3cb97936846345d8a069f455781190
MartinaZandio <159145221+MartinaZandio@users.noreply.github.com>
2024-03-06 21:11:03 +0000

    main

--- ZOOLÓGICO/src/main/java/com/example/App.java
+++ ZOOLÓGICO/src/main/java/com/example/App.java
@@ -1,13 +1,15 @@
-package com.example;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+    }
+
+    public static menu(){
+        System.out.println("1.AÑADIR ANIMAL\n2.AÑADIR HABITAT\n3.VISITANTE....");
+        System.out.println("\n¿Qué desea hacer?");
+        Scanner opcion = new Scanner(System.in);
+        int opcion = opcion.nextInt();
+        
     }
 }
